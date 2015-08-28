a8a69bd7607122fdd768df93811cd6d7a9f61377
Sean Hu <huhuaishun@gmail.com>
2015-08-28 19:13:03 +0800

    sync work

--- src/ViewController.js
+++ src/ViewController.js
@@ -9,4 +9,28 @@
  */
 define(function(require, exports, module) {
 
+    var AnimationController = require('./AnimationController');
+    var TouchSync = require('famous/inputs/TouchSync');
+    var StateModifier = require('famous/modifiers/StateModifier');
+
+    /**
+     * @class
+     * @param {Object} [options] Configurable options.
+     * @alias module:ViewController
+     */
+    function ViewController(options) {
+        AnimationController.apply(this, arguments);
+
+        this._trackingPointer = undefined;
+        this._touchSync = new TouchSync();
+        this._stateModifier = new StateModifier();
+
+        this._eventInput.pipe(this._touchSync);
+        this._touchSync.on('start', ...);
+        this._touchSync.on('update', ...);
+        this._touchSync.on('end', ...);
+    }
+    ViewController.prototype = Object.create(AnimationController.prototype);
+    ViewController.prototype.constructor = ViewController;
+
 });
